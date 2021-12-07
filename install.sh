31aa82190b99c93f9cd6c6346bef6e72dba30c35
bsncubed <ben@apointless.space>
2021-12-08 00:30:22 +1100

    added wget

--- install.sh
+++ install.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 
 #First Run only
-sudo apt install curl vlc -y
+sudo apt install vlc -y
 
 sudo mkdir /etc/minion/
 sudo chown -R $USER /etc/minion/
+wget  https://minion.apointless.space/downloads/minion.sh 
+wget  https://minion.apointless.space/downloads/kill-minion.sh
 wget  
